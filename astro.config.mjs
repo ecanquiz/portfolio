d5126f862f13707dbd4f42e3a601916bf58d1b73
Ernesto Canquiz <fdsoil123@gmail.com>
2023-02-15 20:06:03 -0500

    feat: Add astro-i18next

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import astroI18next from "astro-i18next";
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    astroI18next(),
     tailwind({
       config: { applyBaseStyles: true }
     }),
