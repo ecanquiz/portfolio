1215b6209928f103de0507b5fdf2fc49954ea36b
Ernesto Canquiz <fdsoil123@gmail.com>
2023-02-16 21:14:37 -0500

    wip: Trying to change language dynamically

---
+++ src/components/SelectLanguage.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {ref} from 'vue'
+import i18next from "i18next";
+
+const xyz = ref('')
+i18next.changeLanguage("es");
+</script>
+<template>
+<div class="mx-1">{{xyz}}
+  <select v-model="xyz">{{xyz}}
+    <option value="en">en</option>
+    <option value="es">es</option>
+  </select>
+</div>
+</template>
